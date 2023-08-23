04bf14d04e67ad3440a249b8a961ce065dc2c281
Jannis Roloff <jannis-rolli@msn.com>
2023-08-23 13:58:16 +0200

    next

--- server.js
+++ server.js
@@ -1,38 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
-const NOTES_FILE = 'notes.json';
+const NOTES_FILE = "notes.json";
 
 app.use(bodyParser.json());
 
 // Endpunkt zum Abrufen von Notizen
-app.get('/notes', (req, res) => {
-    if (fs.existsSync(NOTES_FILE)) {
-        const notes = fs.readFileSync(NOTES_FILE, 'utf8');
-        res.json(JSON.parse(notes));
-    } else {
-        res.json([]);
-    }
+app.get("/notes", (req, res) => {
+  if (fs.existsSync(NOTES_FILE)) {
+    const notes = fs.readFileSync(NOTES_FILE, "utf8");
+    res.json(JSON.parse(notes));
+  } else {
+    res.json([]);
+  }
 });
 
 // Endpunkt zum Hinzufügen von Notizen
-app.post('/notes', (req, res) => {
-    const newNote = req.body;
-    let notes = [];
+app.post("/notes", (req, res) => {
+  const newNote = req.body;
+  let notes = [];
 
-    if (fs.existsSync(NOTES_FILE)) {
-        notes = JSON.parse(fs.readFileSync(NOTES_FILE, 'utf8'));
-    }
+  if (fs.existsSync(NOTES_FILE)) {
+    notes = JSON.parse(fs.readFileSync(NOTES_FILE, "utf8"));
+  }
 
-    notes.push(newNote);
-    fs.writeFileSync(NOTES_FILE, JSON.stringify(notes));
+  notes.push(newNote);
+  fs.writeFileSync(NOTES_FILE, JSON.stringify(notes));
 
-    res.json({ status: 'success' });
+  res.json({ status: "success" });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.use(cors());
